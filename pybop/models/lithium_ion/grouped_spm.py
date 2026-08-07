Fix reaction rate calculation

pybop/models/lithium_ion/grouped_spm.py
@@ -488,12 +488,6 @@ class GroupedSPM(BaseGroupedModel):
         b_n = param["Negative electrode Bruggeman coefficient (electrolyte)"]
         Cdl_p = param["Positive electrode double-layer capacity [F.m-2]"]
         Cdl_n = param["Negative electrode double-layer capacity [F.m-2]"]
-        m_p = param.evaluate(
-            param["Positive electrode exchange-current density [A.m-2]"](1, 1, 2, T)
-        )  # (A/m2)(m3/mol)**1.5
-        m_n = param.evaluate(
-            param["Negative electrode exchange-current density [A.m-2]"](1, 1, 2, T)
-        )  # (A/m2)(m3/mol)**1.5
         sigma_p = (
             param["Positive electrode conductivity [S.m-1]"]
             * alpha_p ** param["Positive electrode Bruggeman coefficient (electrode)"]
@@ -509,6 +503,18 @@ class GroupedSPM(BaseGroupedModel):
         epsilon_sep = param["Separator porosity"]
         b_sep = param["Separator Bruggeman coefficient (electrolyte)"]
         kappa_e = param["Electrolyte conductivity [S.m-1]"]  # (ce0, T)
+
+        # Get reaction rates
+        m_p = param.evaluate(
+            param["Positive electrode exchange-current density [A.m-2]"](
+                ce0, c_max_p / 2, c_max_p, T
+            )
+        ) * (2 / (c_max_p * np.sqrt(ce0)))  # (A/m2)(m3/mol)**1.5
+        m_n = param.evaluate(
+            param["Negative electrode exchange-current density [A.m-2]"](
+                ce0, c_max_n / 2, c_max_n, T
+            )
+        ) * (2 / (c_max_n * np.sqrt(ce0)))  # (A/m2)(m3/mol)**1.5
 
         # Compute the cell area and thickness
         A = param["Electrode height [m]"] * param["Electrode width [m]"]

pybop/models/lithium_ion/sp_diffusion.py
@@ -239,9 +239,14 @@ class SPDiffusion(BaseGroupedModel):
         L_p = param["Positive electrode thickness [m]"]
         R_p = param["Positive particle radius [m]"]
         D_p = param["Positive particle diffusivity [m2.s-1]"]
+
+        # Get reaction rate
+        ce0 = param["Initial concentration in electrolyte [mol.m-3]"]
         m_p = param.evaluate(
-            param["Positive electrode exchange-current density [A.m-2]"](1, 1, 2, T)
-        )  # (A/m2)(m3/mol)**1.5
+            param["Positive electrode exchange-current density [A.m-2]"](
+                ce0, c_max_p / 2, c_max_p, T
+            )
+        ) * (2 / (c_max_p * np.sqrt(ce0)))  # (A/m2)(m3/mol)**1.5
 
         # Compute the cell area
         A = param["Electrode height [m]"] * param["Electrode width [m]"]
@@ -263,7 +268,6 @@ class SPDiffusion(BaseGroupedModel):
 
         # Estimate the series resistance, neglecting conductivity losses
         RT_F = pybamm.constants.R.value * param["Ambient temperature [K]"] / F
-        ce0 = param["Initial concentration in electrolyte [mol.m-3]"]
         tau_ct_p = F * R_p / (m_p * np.sqrt(ce0))
         Rct_typ = (2 * RT_F * tau_ct_p) / (3 * Q_th_p)
         R0 = Rct_typ + param["Contact resistance [Ohm]"]